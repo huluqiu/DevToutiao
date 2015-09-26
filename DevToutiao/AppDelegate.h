e9739b70a7e4362a0cb1b81134ed8bd693922bcc
alezai <alezaizai@163.com>
2015-09-26 11:15:33 +0800

    init

---
+++ DevToutiao/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DevToutiao
+//
+//  Created by alezai on 15/9/26.
+//  Copyright © 2015年 alezai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DevToutiao/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DevToutiao
+//
+//  Created by alezai on 15/9/26.
+//  Copyright © 2015年 alezai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
